1a975720a1777e3dd57ab2afda1ada5f84a277ae
cadinsl <cadinsl@gmail.com>
2025-11-15 21:30:38 -0500

    Added mii and audi singleton

---
+++ Assets/Scripts/AudioEventSingleton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class AudioEventSingleton : MonoBehaviour
+{
+    private static AudioEventSingleton _instance;
+
+    public static AudioEventSingleton Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
